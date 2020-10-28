03d8b482fead43368fa982b4c0892bb5ee2d6132
Wonha Shin <wonhashin26@gmail.com>
2020-10-28 20:49:54 +0900

    Init

---
+++ cmake-build-debug/CMakeFiles/chuck_duckhoo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chuck_duckhoo.dir/main.cpp.o"
+  "chuck_duckhoo"
+  "chuck_duckhoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chuck_duckhoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
